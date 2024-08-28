2146cf99fc063452ab5cce605cad7d68b606ee91
Andreas Beasley <andreas@sapioweb.com>
2024-08-28 16:23:37 -0500

    using standalone

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
   plugins: {
     tailwindcss: {},
   },
